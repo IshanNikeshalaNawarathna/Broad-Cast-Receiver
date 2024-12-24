e58c77958865cb5a273a065d15f6a28b55bdce46
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2024-12-25 03:18:21 +0530

    broad cast receiver registations and move norther activity in Broad cast receiver

--- app/src/main/java/com/example/broadcastresiver/BroadCast.java
+++ app/src/main/java/com/example/broadcastresiver/BroadCast.java
@@ -3,11 +3,16 @@ package com.example.broadcastresiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class BroadCast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("BroadCast","Hello Hi!");
+
+        boolean state = intent.getBooleanExtra("state",false);
+        if(state){
+            Intent newIntent = new Intent(context,BroadCastActivity.class);
+            context.startActivity(newIntent);
+        }
+
     }
 }
--- app/src/main/java/com/example/broadcastresiver/BroadCastActivity.java
+++ app/src/main/java/com/example/broadcastresiver/BroadCastActivity.java
@@ -1,7 +1,5 @@
 package com.example.broadcastresiver;
 
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    BroadCast broadCast;
+public class BroadCastActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_broad_cast);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        broadCast = new BroadCast();
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
-        registerReceiver(broadCast,intentFilter);
-
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(broadCast);
     }
 }
